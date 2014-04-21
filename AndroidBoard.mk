9c14ef379fcf58bcb1da833da217b73d123af7be
Shiraz Hashim <shashim@codeaurora.org>
2014-04-21 16:21:03 +0530

    msm8916_512: Enable ZRAM device
    Enable ZRAM as a swap device on system bootup to gain
    performance in low memory conditions. This also configures
    the ZRAM swap disk size to 192M.
    
    To enable ZRAM only for 512MB 8916 platforms, let
    msm8916_512 have its own 'fstab.qcom' file rather than
    sharing from msm8916_32 configuration.
    
    CRs-fixed: 628841
    Change-Id: I5c4a6b14b2cb2a4ea3e9a4ae3f01ecbff73310ee

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -64,7 +64,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ../msm8916_32/fstab.qcom
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
