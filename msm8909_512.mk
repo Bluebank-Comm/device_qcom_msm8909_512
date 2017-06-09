737a2db56e4ba3d129e2873660c1b35321e37d3b
Yogendra Charya Tangalapally <yogendra@codeaurora.org>
2017-06-09 15:45:22 +0530

    Changes to enable Torch
    Change-Id: Ic6be0634dcc6246fdde9c26b0bbec23d6d3d946f

--- msm8909_512.mk
+++ msm8909_512.mk
@@ -2,6 +2,10 @@ $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
        device/qcom/msm8909_512/mixer_paths_qrd_skub.xml:system/etc/mixer_paths_qrd_skub.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.kaios.torch_node=/sys/class/leds/flashlight/brightness \
+       ro.kaios.torch_enable_value=127
+
 $(call inherit-product, device/qcom/msm8909/msm8909.mk)
 
 #QTIC flag
